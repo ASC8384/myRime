da86813eae2ff4a4e52c70f498bfe7662e0fadb6
ASC_8384 <ASC_8384@foxmail.com>
2024-01-22 21:24:51 +0800

    Add: Update Lua files

--- rime.lua
+++ rime.lua
@@ -1,3 +1,21 @@
+
+-- 增加 KeySequence(string)
+-- 1
+--    local ks= KeySequence()
+--    ks:parse("abc")
+-- 2
+--    local ks= KeySequence("abc")
+--
+local _KeySequence=KeySequence
+KeySequence = function(str)
+  local ks = _KeySequence()
+  if type(str)== "string" then
+    ks:parse(str)
+  end
+  return ks
+end
+
+
 --[[
 librime-lua 样例
 
@@ -67,3 +85,24 @@ single_char_filter = require("single_char")
 -- reverse_lookup_filter: 依地球拼音为候选项加上带调拼音的注释
 -- 详见 `lua/reverse.lua`
 reverse_lookup_filter = require("reverse")
+
+--use wildcard to search code
+expand_translator = require("expand_translator")
+
+
+-- III. processors:
+
+-- switch_processor: 通过选择自定义的候选项来切换开关（以简繁切换和下一方案为例）
+-- 详见 `lua/switch.lua`
+switch_processor = require("switch")
+
+-- 由lua 導入 engine/下的組件 processor segmentor translator filters
+-- 生成一個processor 於自己 schema speller 取得 config
+-- processor=Component.Processor(env.engine,"","speller")
+-- 生成一每translator 由 luna_pinyin.schema:/translator 取得 translator config
+--
+-- tran = Component.Translator(env.engine,Schema('luna_pinyin'),"","script_translator")
+--
+-- 配合 test.schema.yaml
+--require 'component_test'
+
